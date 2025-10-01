6c4169d091ac3812cc631e33d8b9accb26c1e63a
Amaka <amakajoseph48@gmail.com>
2025-10-01 09:47:08 +0100

    Try using parent element to delete btn

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ let input = document.getElementById('input-box')
 addBtn.addEventListener('click', addItem)
 function addItem() {
     //if input is empty
-    if (input.value = '') {
+    if (input.value == '') {
         alert('You have not typed anything')
 
 
     } else{
         let li = document.createElement('li')
+        li.style.listStyle = 'none'
 
         //dreating and appending checkbox to li
         let checkbox = document.createElement('input')
         checkbox.type = 'checkbox'
         checkbox.id = 'checkbox'
+        checkbox.style.marginRight = '1.5em'
 
         checkbox.addEventListener('change', toggle)
         function toggle() {
@@ -29,5 +31,49 @@ function addItem() {
         li.appendChild(checkbox)
 
         //appending input value to li
+        let liText = document.createTextNode(input.value)
+        li.appendChild(liText)
+
+        //creating and appending delete button
+        let delBtn = document.createElement('button')
+        delBtn.id = 'delBtn'
+        delBtn.className = 'button delete'
+        delBtn.style.padding = '.2em'
+        delBtn.style.backgroundColor = 'red'
+        delBtn.style.color = 'white'
+        // delBtn.style.cssFloat = 'inline-end'
+        delBtn.style.borderRadius = '3px'
+        delBtn.style.border = 'none'
+        delBtn.style.outline = 'none'
+        delBtn.style.marginLeft = '8em'
+        li.appendChild(delBtn)
+
+        //delBtn text
+        let delBtnTxt = document.createTextNode('X')
+        delBtn.appendChild(delBtnTxt)
+
+        ul.appendChild(li)
+
+        delBtn.addEventListener('click', removeItem)
+        function removeItem(e) {
+            if (confirm('Are you sure?')) {
+                e.target.parentElement.remove()
+            }
+        }
     }
+
+    input.value = ''
 }
+
+//remove delete button
+// ul.addEventListener('click', remItem)
+// function remItem(e) {
+//     if(e.target.classList.contains('delete')) {
+//         if(confirm('Are you sure?')) {
+//             // e.target.parentElement.remove()
+//             let li = e.target.parentElement
+//             ul.removeChild(li)
+//         }
+//     }
+// }
+
